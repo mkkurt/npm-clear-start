0d600328d0c9dff05ad1e71a1e167249dd7999cd
Kutay Kurt <kutaykurt99@gmail.com>
2023-12-07 19:28:48 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # npm-clear-start
-Run both commands with one click of a button.
+
+# Run both commands with one click of a button.
+
+This extension is for developers who use npm start and clear commands in their terminal. It allows you to run both commands with one click of a button.
+
+## Features
+
+This extension adds a button to the status bar that will run the npm start and clear commands in the terminal.
+
+One button runs only the npm start command, the other button clears the terminal and then runs the npm start command.
+
+## Extension Settings
+
+    "command": "noderunaddon.runStartCommand",
+    "key": "ctrl+shift+enter"
+
+    "command": "noderunaddon.clearAndRunStartCommand",
+    "key": "ctrl+shift+alt+enter"
+
+**Enjoy!**
---
+++ extension.js
@@ -0,0 +1,68 @@
+const vscode = require("vscode");
+
+let terminal;
+
+function activate(context) {
+  console.log("Extension 'npm-clear-start' is now active!");
+  // Create a new terminal instance on extension launch if there are no open terminals
+  if (vscode.window.terminals.length === 0) {
+    terminal = vscode.window.createTerminal();
+    terminal.show();
+  } else {
+    terminal = vscode.window.activeTerminal || vscode.window.terminals[0];
+  }
+
+  // Create status bar items for the terminal
+  const startButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    100
+  );
+  startButton.text = "$(play) Run Start";
+  startButton.command = "noderunaddon.runStartCommand";
+  startButton.show();
+
+  const clearAndStartButton = vscode.window.createStatusBarItem(
+    vscode.StatusBarAlignment.Right,
+    99
+  );
+  clearAndStartButton.text = "$(play) Clear and Run Start";
+  clearAndStartButton.command = "noderunaddon.clearAndRunStartCommand";
+  clearAndStartButton.show();
+
+  // Register the command to run the start script
+  let runStartDisposable = vscode.commands.registerCommand(
+    "noderunaddon.runStartCommand",
+    () => {
+      console.log("Run npm start");
+      terminal.sendText("npm start");
+    }
+  );
+
+  // Register the command to run the second-latest command
+  let runClearAndStartDisposable = vscode.commands.registerCommand(
+    "noderunaddon.clearAndRunStartCommand",
+    () => {
+      console.log("Clear terminal (clear) and run npm start");
+      terminal.sendText("clear");
+      terminal.sendText("npm start");
+    }
+  );
+
+  // Add the disposables to the context subscriptions
+  context.subscriptions.push(startButton);
+  context.subscriptions.push(clearAndStartButton);
+  context.subscriptions.push(runStartDisposable);
+  context.subscriptions.push(runClearAndStartDisposable);
+
+  // Register an event listener for terminal creation
+  vscode.window.onDidOpenTerminal((event) => {
+    terminal = event;
+  });
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
+};
